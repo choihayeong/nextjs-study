688161e5a2af610a094ab926515ca985a75e29b2
DESKTOP-D0CK6V5\CTTD <hychoi318@gmail.com>
2025-09-10 13:47:03 +0900

    2-3 / navigating for page routing

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+    // router.replace("/test"); // 현재 페이지를 대체함(뒤로가기 했을 때, 현재 페이지로 돌아올 수 없음)
+    // router.back(); // 이전 페이지로 이동
+    // router.prefetch("/test"); // 사전 로드(미리 로드)
+    // router.reload(); // 현재 페이지 새로고침
+  }
+
   return (
-    <div>  
+    <div>
       <header>
-        글로벌 헤더
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button type="button" onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
       </header>
       <Component {...pageProps} />
     </div>
