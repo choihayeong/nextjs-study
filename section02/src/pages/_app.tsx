53646586bf9ddaee83a8c9a6bb55ed867c5d485d
DESKTOP-D0CK6V5\CTTD <hychoi318@gmail.com>
2025-09-10 16:07:17 +0900

    2-4 / Prefetching

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -14,12 +15,16 @@ export default function App({ Component, pageProps }: AppProps) {
     // router.reload(); // 현재 페이지 새로고침
   }
 
+  useEffect(() => {
+    router.prefetch("/test"); // 사전 로드(미리 로드)
+  }, []) // 한 번만 실행 시키므로 의존성 배열을 빈 배열로 둠
+
   return (
     <div>
       <header>
         <Link href={"/"}>index</Link>
         &nbsp;
-        <Link href={"/search"}>search</Link>
+        <Link href={"/search"} prefetch={false}>search</Link>
         &nbsp;
         <Link href={"/book/1"}>book/1</Link>
         <div>
