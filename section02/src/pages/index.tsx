c78ab6d921eb2d8f986044ff348e6476e0c7bb1e
DESKTOP-D0CK6V5\CTTD <hychoi318@gmail.com>
2025-09-16 13:35:12 +0900

    2-6 / Styling

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,10 @@
+import style from "./index.module.css";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>Heading #1</h1>
+      <h2 className={style.h2}>Heading #2</h2>
+    </>
+  );
 }
